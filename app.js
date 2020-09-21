d9e658bb7d9ac508a78e465bf732c4c158256bda
Connor McKenna <connormckenna2654@gmail.com>
2020-09-21 10:37:55 -0400

    added framework!

---
+++ app.js
@@ -0,0 +1,12 @@
+async function getMonster(monster) {
+  try {
+    const url = `https://www.dnd5eapi.co/api/monsters/${monster}`
+    const response = await axios.get(url)
+    console.log(response.data)
+  } catch (error) {
+    console.log(`Error: ${error}`)
+
+  }
+}
+
+getMonster('goblin')
