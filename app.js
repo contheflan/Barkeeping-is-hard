fab76b2c41b289884f4d05709f608314b574ecf5
Connor McKenna <connormckenna2654@gmail.com>
2020-09-22 17:14:20 -0400

    finished mvps!

--- app.js
+++ app.js
@@ -1,31 +1,49 @@
+let monsterArmor = 0
+
 async function getMonster(monster) {
   try {
     const url = `https://www.dnd5eapi.co/api/monsters/${monster}`
     const response = await axios.get(url)
-    const monsterApi = document.querySelector(".api-info")
+    const monsterApi = document.querySelector("#api-info")
     const monsterIntro = document.querySelector("#intro-text")
-    monsterIntro.innerText = `Zounds! A ${response.data.name} in your bar! Roll a 20-sided dice to attack! If the number rolled is higher than their armor you win! Show no quarter!`
-    monsterApi.innerText = `Name: ${response.data.name} \n \n Armor Class: ${response.data.armor_class}`
+    monsterIntro.innerText = `Zounds! A ${response.data.name} in your bar! Roll a 20-sided dice to throw 'em out! If the number rolled is equal to or higher than their armor you win! Show no quarter!`
+    monsterApi.innerText = `Name: ${response.data.name} \n \n Armor: ${response.data.armor_class}`
+    monsterArmor = response.data.armor_class
   } catch (error) {
     console.log(`Error: ${error}`)
   }
 }
-getMonster('commoner')
+getMonster('goblin')
 
-// add more monsters! goblin, mimic, death knight, imp
-
+// add more monsters! commoner, goblin, mimic, satyr
+let diceRoll = 0
 function roll(min, max) {
-  return Math.round(Math.random() * (max - min) + min)
+  let temp = Math.round(Math.random() * (max - min) + min)
+  diceRoll = temp
+  return temp
 }
 
 const button = document.querySelector('#dice-roll');
+const diceResult = document.querySelector("#results")
+
 
 button.addEventListener('click', () => {
-  // console.log(roll(1, 20))
-  const diceResult = document.querySelector(".results")
-  // create an if statement for comparing roll and monster ac
   diceResult.innerText = roll(1, 20)
-  // if (roll > ${ response.data.name })
+  compare(diceRoll, monsterArmor)
 })
 
 
+function compare(diceRoll, monsterArmor) {
+  let loser = " \n LOSER"
+  let winner = " \n WINNER"
+  if (diceRoll >= monsterArmor) {
+    document.querySelector('#results').style.color = "green"
+    diceResult.innerText += winner
+  } else {
+    document.querySelector('#results').style.color = "red"
+    diceResult.innerText += loser
+  }
+}
+
+
+// to do: add splash/results screen, dice animation, weapons with dice modifiers, more monsters, add images, health bar?
