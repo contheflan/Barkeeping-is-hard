5a931b7aa70764e5dc6581ce5f4bb3d0e4880882
Connor McKenna <connormckenna2654@gmail.com>
2020-09-22 12:29:59 -0400

    added api functionality!

--- app.js
+++ app.js
@@ -2,32 +2,28 @@ async function getMonster(monster) {
   try {
     const url = `https://www.dnd5eapi.co/api/monsters/${monster}`
     const response = await axios.get(url)
-    // console.log(response.data.name)
+    const monsterApi = document.querySelector(".api-info")
+    const monsterIntro = document.querySelector("#intro-text")
+    monsterIntro.innerText = `Zounds! A ${response.data.name} in your bar! Roll a 20-sided dice to attack! If the number rolled is higher than their armor you win! Show no quarter!`
+    monsterApi.innerText = `${response.data.name} \n Armor Class: ${response.data.armor_class}`
   } catch (error) {
     console.log(`Error: ${error}`)
-
   }
 }
-
-getMonster('goblin')
-
-// const dice = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+getMonster('commoner')
 
 function roll(min, max) {
   return Math.round(Math.random() * (max - min) + min)
 }
-// console.log(roll(1, 20))
-//
 
 const button = document.querySelector('#dice-roll');
 
 button.addEventListener('click', () => {
   // console.log(roll(1, 20))
   const diceResult = document.querySelector(".results")
-  diceResult.innerHTML = roll(1, 20)
+  // create an if statement for comparing roll and monster ac
+  diceResult.innerText = roll(1, 20)
+  // if (roll > ${ response.data.name })
 })
 
-// button.addEventListener('click', () => {
-//   const diceResult = document.querySelector(".results")
-//   diceResult.innerHTML = roll(1, 20)
-// })
+
