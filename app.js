f0bd7b233fe420991352714e2fba50fcd2374b6e
Connor McKenna <connormckenna2654@gmail.com>
2020-09-22 11:36:11 -0400

    added button functionality!

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ async function getMonster(monster) {
   try {
     const url = `https://www.dnd5eapi.co/api/monsters/${monster}`
     const response = await axios.get(url)
-    console.log(response.data)
+    // console.log(response.data.name)
   } catch (error) {
     console.log(`Error: ${error}`)
 
@@ -11,4 +11,23 @@ async function getMonster(monster) {
 
 getMonster('goblin')
 
-const roll = []
+// const dice = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+function roll(min, max) {
+  return Math.round(Math.random() * (max - min) + min)
+}
+// console.log(roll(1, 20))
+//
+
+const button = document.querySelector('#dice-roll');
+
+button.addEventListener('click', () => {
+  // console.log(roll(1, 20))
+  const diceResult = document.querySelector(".results")
+  diceResult.innerHTML = roll(1, 20)
+})
+
+// button.addEventListener('click', () => {
+//   const diceResult = document.querySelector(".results")
+//   diceResult.innerHTML = roll(1, 20)
+// })
