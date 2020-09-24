a61f8350dbfea400e217fd6c60e59a93e7dba9f5
Connor McKenna <connormckenna2654@gmail.com>
2020-09-24 14:20:20 -0400

    added more media queries, styling, moving to localize imgs

--- app.js
+++ app.js
@@ -2,15 +2,28 @@ let monsterApi = document.querySelector("#api-info")
 let monsterName = ""
 let monsterArmor = 0
 let monsterHealth = 0
-let theMonsters = ['goblin', 'mimic', 'skeleton', 'ogre', 'doppelganger']
+let goblin = { name: 'goblin', img: 'https://static.wikia.nocookie.net/diablo_gamepedia/images/4/48/Warped_One_%28Diablo_II%29.gif/revision/latest?cb=20090323220525' }
+let skeleton = { name: 'skeleton', img: 'https://static.wikia.nocookie.net/diablo_gamepedia/images/1/11/Bone_Warrior_%28Diablo_II%29.gif/revision/latest?cb=20090408135450' }
+let mimic = { name: 'mimic', img: 'https://static.wikia.nocookie.net/diablo_gamepedia/images/9/91/Siege_Beast_%28Diablo_II%29.gif/revision/latest?cb=20091107201227' }
+let ogre = { name: 'ogre', img: 'https://static.wikia.nocookie.net/diablo_gamepedia/images/f/fc/Hephasto_the_Armorer_%28Diablo_II%29.gif/revision/latest?cb=20101010213045' }
+let zombie = { name: 'zombie', img: 'https://static.wikia.nocookie.net/diablo_gamepedia/images/6/66/Zombie_%28Diablo_II%29.gif/revision/latest?cb=20090330212712' }
+let swarm = { name: 'swarm-of-insects', img: 'https://static.wikia.nocookie.net/diablo_gamepedia/images/8/8a/Hell_Swarm_%28Diablo_II%29.gif/revision/latest?cb=20090428220720' }
+let vampire = { name: 'vampire', img: 'https://static.wikia.nocookie.net/diablo_gamepedia/images/e/e9/Ghoul_Lord_%28Diablo_II%29.gif/revision/latest?cb=20101011065645' }
+let chain = { name: 'chain-devil', img: 'https://static.wikia.nocookie.net/diablo_gamepedia/images/6/64/Moon_Lord_%28Diablo_II%29.gif/revision/latest?cb=20090625165259' }
+let horn = { name: 'horned-devil', img: 'https://static.wikia.nocookie.net/diablo_gamepedia/images/9/98/Balrog_%28Diablo_II%29.gif/revision/latest?cb=20090322162229' }
 
+
+let theMonsters = [goblin, skeleton, mimic, ogre, zombie, swarm, vampire, chain, horn]
 async function getMonster(monster) {
   try {
-    const url = `https://www.dnd5eapi.co/api/monsters/${monster[Math.floor(Math.random() * theMonsters.length)]}`
+    const monsterObject = monster[Math.floor(Math.random() * theMonsters.length)]
+    const url = `https://www.dnd5eapi.co/api/monsters/${monsterObject.name}`
     const response = await axios.get(url)
     const monsterIntro = document.querySelector("#intro-text")
     monsterIntro.innerText = `Zounds! A ${response.data.name} in your bar! Roll a 20-sided dice to throw 'em out! If the number rolled is equal to or higher than their armor you win! Show no quarter!`
     monsterApi.innerText = `NAME: ${response.data.name} \n \n ARMOR: ${response.data.armor_class} \n \n HEALTH: ${response.data.hit_points}`
+    const image = document.querySelector("#monster-img")
+    image.setAttribute("src", monsterObject.img)
     monsterName = response.data.name
     monsterArmor = response.data.armor_class
     monsterHealth = response.data.hit_points
@@ -36,8 +49,8 @@ button.addEventListener('click', () => {
 })
 
 function compare(diceRoll) {
-  let loser = " \n LOSER -10 DMG"
-  let winner = " \n WINNER -10 DMG"
+  let loser = " \n LOSER \n -10 HP"
+  let winner = " \n WINNER \n 10 DMG"
   let health = document.querySelector('#health-bar')
   if (diceRoll >= monsterArmor) {
     document.querySelector('#results').style.color = "limegreen"
@@ -57,11 +70,4 @@ function compare(diceRoll) {
   }
 }
 
-// function KeyboardEvent() {
-//   window.location.replace("intro.html")
-// }
-
-
-// create a if statement to kick you to various results screens
-
-// to do: add results screen, dice animation, weapons with dice modifiers, add images?
+// to do: add dice animation, weapons with dice modifiers?
