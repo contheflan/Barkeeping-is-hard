49f6db787b5496920301b1e80bb7d8056a6e2265
Connor McKenna <connormckenna@Connors-MacBook-Pro.local>
2020-09-24 15:58:26 -0400

    localized assets

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ let monsterApi = document.querySelector("#api-info")
 let monsterName = ""
 let monsterArmor = 0
 let monsterHealth = 0
-let goblin = { name: 'goblin', img: 'https://static.wikia.nocookie.net/diablo_gamepedia/images/4/48/Warped_One_%28Diablo_II%29.gif/revision/latest?cb=20090323220525' }
-let skeleton = { name: 'skeleton', img: 'https://static.wikia.nocookie.net/diablo_gamepedia/images/1/11/Bone_Warrior_%28Diablo_II%29.gif/revision/latest?cb=20090408135450' }
-let mimic = { name: 'mimic', img: 'https://static.wikia.nocookie.net/diablo_gamepedia/images/9/91/Siege_Beast_%28Diablo_II%29.gif/revision/latest?cb=20091107201227' }
-let ogre = { name: 'ogre', img: 'https://static.wikia.nocookie.net/diablo_gamepedia/images/f/fc/Hephasto_the_Armorer_%28Diablo_II%29.gif/revision/latest?cb=20101010213045' }
-let zombie = { name: 'zombie', img: 'https://static.wikia.nocookie.net/diablo_gamepedia/images/6/66/Zombie_%28Diablo_II%29.gif/revision/latest?cb=20090330212712' }
-let swarm = { name: 'swarm-of-insects', img: 'https://static.wikia.nocookie.net/diablo_gamepedia/images/8/8a/Hell_Swarm_%28Diablo_II%29.gif/revision/latest?cb=20090428220720' }
-let vampire = { name: 'vampire', img: 'https://static.wikia.nocookie.net/diablo_gamepedia/images/e/e9/Ghoul_Lord_%28Diablo_II%29.gif/revision/latest?cb=20101011065645' }
-let chain = { name: 'chain-devil', img: 'https://static.wikia.nocookie.net/diablo_gamepedia/images/6/64/Moon_Lord_%28Diablo_II%29.gif/revision/latest?cb=20090625165259' }
-let horn = { name: 'horned-devil', img: 'https://static.wikia.nocookie.net/diablo_gamepedia/images/9/98/Balrog_%28Diablo_II%29.gif/revision/latest?cb=20090322162229' }
+let goblin = { name: 'goblin', img: 'assets/goblin.gif' }
+let skeleton = { name: 'skeleton', img: 'assets/skeleton.gif' }
+let mimic = { name: 'mimic', img: 'assets/mimic.gif' }
+let ogre = { name: 'ogre', img: 'assets/ogre.gif' }
+let zombie = { name: 'zombie', img: 'assets/zombie.gif' }
+let swarm = { name: 'swarm-of-insects', img: 'assets/swarm.gif' }
+let vampire = { name: 'vampire', img: 'assets/vampire.gif' }
+let chain = { name: 'chain-devil', img: 'assets/chain.gif' }
+let horn = { name: 'horned-devil', img: 'assets/horn.gif' }
 
 
 let theMonsters = [goblin, skeleton, mimic, ogre, zombie, swarm, vampire, chain, horn]
